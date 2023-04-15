2c00a1dfbe26a4ee9eb1039b1d725d6c930a6699
jakegodsall <jake.edward.godsall@gmail.com>
2023-04-15 10:10:26 +0100

    Added platform buttons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, Image, Heading, CardBody } from '@chakra-ui/react';
+import { Card, Image, Heading, CardBody, Text } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
 
 interface GameCardProps {
     game: Game;
 }
 
 const GameCard = ({ game }: GameCardProps) => {
+    console.log(game.parent_platforms);
     return (
         <Card borderRadius='10px' overflow='hidden'>
             <Image src={game.background_image} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
+                <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
             </CardBody>
         </Card>
     );
